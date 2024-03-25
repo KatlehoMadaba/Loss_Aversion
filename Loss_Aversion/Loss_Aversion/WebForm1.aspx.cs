fa28df089f231abb9fbd05f8a187c249e2597d22
KatlehoMadaba <katlehomadaba44@gmail.com>
2024-03-26 00:02:18 +0200

    For Development

---
+++ Loss_Aversion/Loss_Aversion/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Loss_Aversion
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnNext_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("WebForm2.aspx");
+        }
+    }
+}
